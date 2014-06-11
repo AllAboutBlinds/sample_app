b0ce63b2239e75d7dfa57eea47e5a363461535d6
Kenny Blackwelder <kennyb@allaboutblinds.net>
2014-06-11 15:43:21 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
-RSpec.configure do |config|
-	config.include Capybara::DSL
-end
+
 
 RSpec.describe "StaticPages", :type => :request do
   describe "Static_pages" do
@@ -12,6 +10,17 @@ RSpec.describe "StaticPages", :type => :request do
     it "should have the content 'Sample App" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+
+      describe "Contact page" do
+
+      	it "should have the content 'Contact'" do
+      		visit '/static_pages/contact'
+      		expect(page).to have_content('Contact')
+      	end
+
+      	it it "should have the title 'Contact'" do
+      		visit '/static_pages/contact'
+      		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
