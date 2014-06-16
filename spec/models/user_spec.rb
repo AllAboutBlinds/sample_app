5ba2f34a0ba8f3762cd6b305a116544422f0776e
Kenny Blackwelder <kennyb@allaboutblinds.net>
2014-06-16 09:38:04 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe User, :type => :model do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+
 
   describe "when email format is invalid" do
     it "should be invalid" do
